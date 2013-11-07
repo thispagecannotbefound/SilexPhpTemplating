93bf5070ed93fa0a76dedf28caafada515216652
Abel de Beer <abel@thispagecannotbefound.com>
2013-11-07 19:02:00 +0100

    Adds full functionality and tests

--- src/ThisPageCannotBeFound/Silex/Provider/PhpTemplatingServiceProvider.php
+++ src/ThisPageCannotBeFound/Silex/Provider/PhpTemplatingServiceProvider.php
@@ -2,21 +2,79 @@
 
 namespace ThisPageCannotBeFound\Silex\Provider;
 
+use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\Templating\Helper\SlotsHelper;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
 
 /**
  * @author Abel de Beer <abel@thispagecannotbefound.com>
  */
 class PhpTemplatingServiceProvider implements ServiceProviderInterface {
 
-	public function boot(\Silex\Application $app) {
+	public function boot(Application $app) {
 
 	}
 
-	public function register(\Silex\Application $app) {
+	public function register(Application $app) {
 		if (!class_exists('Symfony\\Component\\Templating\\PhpEngine')) {
 			throw new \LogicException('You must register the Symfony Templating Component to use the PhpTemplatingServiceProvider');
 		}
+
+		$app['templating.paths'] = $app->share(function() use($app) {
+					return array();
+				});
+
+		$app['templating.helpers'] = $app->share(function() use($app) {
+					return array();
+				});
+
+		$app['templating'] = $app->share(function() use($app) {
+					$engine = $app['templating.engine'];
+
+					// set helpers
+					$helpers = array_merge($app['templating.helpers'],
+							$app['templating.default_helpers']);
+					$engine->setHelpers($helpers);
+
+					// add app global
+					$engine->addGlobal('app', $app);
+
+					return $engine;
+				});
+
+		$app['templating.engine'] = $app->share(function() use($app) {
+					return new PhpEngine($app['templating.parser'], $app['templating.loader']);
+				});
+
+		$app['templating.parser'] = $app->share(function() use($app) {
+					return new TemplateNameParser();
+				});
+
+		$app['templating.loader'] = $app->share(function() use($app) {
+					$paths = (array) $app['templating.paths'];
+					$nameParam = '%name%';
+
+					foreach ($paths as $key => $path) {
+						// add name param if it is missing
+						if (strpos($path, $nameParam) === false) {
+							if (substr($path, -1) !== '/') {
+								$path .= '/';
+							}
+
+							$path .= $nameParam;
+							$paths[$key] = $path;
+						}
+					}
+
+					return new FilesystemLoader($paths);
+				});
+
+		$app['templating.default_helpers'] = $app->share(function() use($app) {
+					return array(new SlotsHelper());
+				});
 	}
 
 }
--- tests/ThisPageCannotBeFound/Silex/Tests/PhpTemplatingServiceProviderTest.php
+++ tests/ThisPageCannotBeFound/Silex/Tests/PhpTemplatingServiceProviderTest.php
@@ -17,7 +17,7 @@ class PhpTemplatingServiceProviderTest extends \PHPUnit_Framework_TestCase {
 		$this->app = new Application();
 	}
 
-	public function testTemplatingComponentNotInstalledThrows() {
+	public function testTemplatingComponentNotInstalledShouldThrow() {
 		$expectedMessage = 'You must register the Symfony Templating Component to use the PhpTemplatingServiceProvider';
 
 		try {
@@ -26,6 +26,25 @@ class PhpTemplatingServiceProviderTest extends \PHPUnit_Framework_TestCase {
 			$this->assertInstanceOf('LogicException', $exception);
 			$this->assertEquals($expectedMessage, $exception->getMessage());
 		}
+	}
+
+	public function testAppGlobalShouldBeAvailable() {
+		$this->app->register(new PhpTemplatingServiceProvider());
+
+		$globals = $this->app['templating']->getGlobals();
+
+		$this->assertSame($this->app, $globals['app']);
+	}
+
+	public function testProviderShouldAddMissingNameParam() {
+		$this->app->register(new PhpTemplatingServiceProvider());
+
+		$this->app['templating.paths'] = __DIR__ . '/templates';
+		
+		$rendered = $this->app['templating']->render('example.php',
+				array('planet' => 'World'));
+
+		$this->assertEquals($rendered, 'Hello, World!');
 	}
 
 }
